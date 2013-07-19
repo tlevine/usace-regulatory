2fb75d317e781e26b24a9dc46a70475509e295ae
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 15:11:51 -0700

    parse locations

---
+++ test/test_parse_locations.py
@@ -0,0 +1,15 @@
+import nose.tools as n
+import lxml.html
+
+import parse
+
+html = lxml.html.parse(os.path.join('fixtures_parse_locations', 'Locations.aspx')).getroot()
+observed = parse.locations(html)
+
+def test_schema():
+    'A row should contain the fields `division`, `district` and `subdomain`.'
+    n.assert_equal(set(observed[0].keys()), {'division', 'district', 'subdomain'})
+
+def test_district_count():
+    'The correct number of districts should be returned.'
+    n.assert_equal(len(set([district['division'] for district in observed])), 9)
