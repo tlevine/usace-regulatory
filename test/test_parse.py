1f3f7850f018d7e6060e14843db8cef1b25c4cd6
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 15:04:17 -0700

    a test

---
+++ test/test_parse.py
@@ -0,0 +1,11 @@
+import nose.tools as n
+import lxml.html
+
+import parse
+
+html = lxml.html.parse('nap-2013-07-19.html').getroot()
+
+def test_count():
+    'The correct number of notices should be returned.'
+    observed = parse.parse(html)
+    n.assert_equal(len(observed['notices']), 5)
