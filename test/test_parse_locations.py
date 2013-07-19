21e713909cda3577f6bdfb6dcf6c2528d97cb6b1
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 15:53:08 -0700

    data test

--- test/test_parse_locations.py
+++ test/test_parse_locations.py
@@ -15,3 +15,16 @@ def test_schema():
 def test_district_count():
     'The correct number of districts should be returned.'
     n.assert_equal(len(observed), 9)
+
+def test_data():
+    n.assert_dict_equal(observed[3], {
+       'districts': [ { 'href': u'http://www.nwk.usace.army.mil', 'text': u'Kansas City District'},
+                      {'href': u'http://www.nwo.usace.army.mil', 'text': u'Omaha District '},
+                      {'href': u'http://www.nwp.usace.army.mil', 'text': u'Portland District '},
+                      {'href': u'http://www.nws.usace.army.mil', 'text': u'Seattle District '},
+                      {'href': u'http://www.nww.usace.army.mil', 'text': u'Walla Walla District '},
+                      {'href': u'http://www.nww.usace.army.mil', 'text': u'Walla Walla District '}
+       ],
+       'href': u'http://www.nwd.usace.army.mil',
+       'text': u'Northwestern Division'
+    })
