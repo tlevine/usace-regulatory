482802ab2b3598b801cdc74071f7278bad5756e3
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 17:23:25 -0700

    oops

--- parse/_public_notice.py
+++ parse/_public_notice.py
@@ -9,10 +9,10 @@ def _expiration_date_raw(html):
     return html.xpath('//div[@class="da_body"]/em/text()')[0].split(': ')[-1]
 
 def _description(html):
-    return html.cssselect('div.da_body > div.da_black')[0].text_content()
+    return html.cssselect('div.da_body > div.da_black')[0].text_content().replace(u'\xa0', '')
 
 def _notice_href(html):
-    return html.xpath('//div[@class="da_body"]/p/strong/a/@href')[0].split('.usace.army.mil')[-1].replace(u'\xa0', '')
+    return html.xpath('//div[@class="da_body"]/p/strong/a/@href')[0].split('.usace.army.mil')[-1]
 
 def _attachment_href(html):
     return chunk[2].xpath('descendant::a/@href')[0]
