9626bf8faaf7522e3c4e71a75c2bacb965bd72de
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 17:18:27 -0700

    fix some tests

--- parse/_public_notice.py
+++ parse/_public_notice.py
@@ -2,18 +2,18 @@ import datetime
 import lxml.html
 import re
 
-def _posted_date_raw(chunk):
-    return chunk[1].xpath('text()')[0].split(':')[0]
+def _posted_date_raw(html):
+    return html.xpath('//div[@class="da_body"]/p/em[@class="da_black"]/text()')[0].replace('Posted: ', '')
 
-def _expiration_date_raw(chunk):
-    return chunk[1].xpath('em/text()')[0].replace('Expiration date: ', '')
+def _expiration_date_raw(html):
+    return html.xpath('//div[@class="da_body"]/em/text()')[0].replace('Posted: ', '')
 
-def _description(chunk):
-    return re.sub(r'^[^:]+: ', '', chunk[1].xpath('text()')[0]).rstrip()
+def _description(html):
+    return re.sub(r'^[^:]+: ', '', html[1].xpath('text()')[0]).rstrip()
 
-def _notice_href(chunk):
-    return chunk[0].xpath('@href')[0].split('.usace.army.mil')[-1]
+def _notice_href(html):
+    return html.xpath('//div[@class="da_body"]/p/strong/a/@href')[0].split('.usace.army.mil')[-1]
 
-def _attachment_href(chunk):
+def _attachment_href(html):
     return chunk[2].xpath('descendant::a/@href')[0]
 
